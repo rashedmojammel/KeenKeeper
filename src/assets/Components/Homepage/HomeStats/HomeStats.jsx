a8c4a2dfc57ae10d102f1afb6a58d32ca239f284
rashedmojammel <rashedmojamme56@gmail.com>
2026-04-15 22:48:43 +0600

    fix : fixed homestats responsive

--- src/assets/Components/Homepage/HomeStats/HomeStats.jsx
+++ src/assets/Components/Homepage/HomeStats/HomeStats.jsx
@@ -7,7 +7,7 @@ const HomeStats = () => {
   console.log(friendStats);
 
     return (
-        <div className='grid grid-cols-4  w-7/12 mx-auto mt-8 flex-wrap justify-center border-b border-gray-300 pb-8'> 
+        <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 w-11/12 md:w-8/12 mx-auto mt-8 border-b border-gray-300 pb-8 mx-auto'> 
             <div className="border-white bg-white shadow-lg rounded-lg flex flex-col items-center justify-center p-2 w-[200px] h-[100px]">
   <h1 className="text-2xl font-bold">7</h1>
   <p className="text-sm text-gray-600">Total Friends</p>
