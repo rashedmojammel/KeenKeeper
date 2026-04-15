3e3cf307bb920537c63569fab1410bbd692ba26d
rashedmojammel <rashedmojamme56@gmail.com>
2026-04-15 09:26:43 +0600

    feat : updated the readme files

--- src/assets/Layout/MainLayout.jsx
+++ src/assets/Layout/MainLayout.jsx
@@ -5,7 +5,7 @@ import Footer from '../Components/Shared/Footer/Footer';
 
 const MainLayout = () => {
     return (
-        <div className='bg-gray-50 '>
+        <div className='bg-gray-50'>
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
